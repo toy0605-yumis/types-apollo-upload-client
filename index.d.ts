365d6e858b029072cc1d6f45adc49bd69dacd6dc
이승렬 <toy0605@yumis.io>
2020-08-05 16:17:16 +0900

    Initial commit

---
+++ index.d.ts
@@ -0,0 +1,21 @@
+// Type definitions for apollo-upload-client 14.0
+// Project: https://github.com/jaydenseric/apollo-upload-client#readme
+// Definitions by: Edward Sammut Alessi <https://github.com/Slessi>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 3.1
+
+import { ApolloLink, HttpOptions } from "@apollo/client";
+
+export { ReactNativeFile } from "extract-files";
+
+declare global {
+    interface GlobalFetch {
+        fetch: WindowOrWorkerGlobalScope["fetch"];
+    }
+}
+
+/**
+ * `createUploadLink` options match `createHttpLink` options
+ * @param linkOptions `HttpOptions`
+ */
+export function createUploadLink(linkOptions?: HttpOptions): ApolloLink;
